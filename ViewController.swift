6e827225ef0cbd6cd316421d9925c83cad384436
김찬민 <rlacksals96@gmail.com>
2020-08-15 23:07:03 +0900

    feat: turn on the light by button

---
+++ ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by Chanmin on 2020/08/15.
+//  Copyright © 2020 kim. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    //Interface Builder
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var flashImageView: UIImageView!
+    
+    var isOn=false
+    var clickSound: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepareSound()
+    }
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    func prepareSound(){
+        let path = Bundle.main.path(forResource: "switch.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            clickSound = try AVAudioPlayer(contentsOf: url)
+            clickSound?.prepareToPlay()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    
+    @IBAction func switchTapped(_ sender: Any) {
+        isOn = !isOn
+        clickSound?.play()
+        toggleTorch(on: isOn)
+//        if isOn==true{
+//            switchbutton.setImage(#imageLiteral(resourceName: "onSwitch"), for: .normal)
+//            flashImageView.image=UIImage(named: "onBG")
+//
+//        }
+//        else{
+//            switchbutton.setImage(#imageLiteral(resourceName: "offSwitch"), for: .normal)
+//            flashImageView.image=#imageLiteral(resourceName: "offBG")
+//
+//        }
+        flashImageView.image=isOn ? #imageLiteral(resourceName: "onBG") :#imageLiteral(resourceName: "offBG")
+        switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
+       
+    }
+    
+}
+
